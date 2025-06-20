a5ed8b1aede8079a2886d0ffcbe2991c971afc5a
tf4482 <tf4482@gmail.com>
2025-06-20 18:36:57 +0200

    refactor: :fire: Removed branding

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('')
             ->login()
+            ->brandName('')
             ->colors([
                 'primary' => Color::Amber,
             ])
