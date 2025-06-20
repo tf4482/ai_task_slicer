00b6d988b5635c00bf9c95200c14f5f54d0920d3
tf4482 <tf4482@gmail.com>
2025-06-20 18:44:32 +0200

    feat: :lipstick: Hid dashboard headline

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -7,4 +7,11 @@ use Filament\Pages\Dashboard as BaseDashboard;
 class Dashboard extends BaseDashboard
 {
     protected static bool $shouldRegisterNavigation = false;
+    
+    protected static ?string $title = '';
+    
+    public function getHeading(): string
+    {
+        return '';
+    }
 }
